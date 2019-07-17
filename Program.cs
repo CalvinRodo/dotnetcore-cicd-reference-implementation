815024654398744a10efb9ed7f01469e02ec95e1
CalvinRodo <calvinv@gmail.com>
2019-07-17 19:47:59 -0400

    Adding CLS Compliant statement

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+[assembly: CLSCompliant(true)]
 
 namespace dotnetcore_cicd_reference_implementation
 {
